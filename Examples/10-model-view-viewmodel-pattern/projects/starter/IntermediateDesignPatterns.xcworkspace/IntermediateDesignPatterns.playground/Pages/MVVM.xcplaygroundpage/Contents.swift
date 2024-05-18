c4104829781f0172597917a09b2d61964bec1a7b
7과11사이 <dongjunlee222@gmail.com>
2024-05-18 23:55:51 +0900

    ADD: 📇 MVVM Sample

--- Examples/10-model-view-viewmodel-pattern/projects/starter/IntermediateDesignPatterns.xcworkspace/IntermediateDesignPatterns.playground/Pages/MVVM.xcplaygroundpage/Contents.swift
+++ Examples/10-model-view-viewmodel-pattern/projects/starter/IntermediateDesignPatterns.xcworkspace/IntermediateDesignPatterns.playground/Pages/MVVM.xcplaygroundpage/Contents.swift
@@ -13,3 +13,103 @@
  
  ## Code Example
  */
+import PlaygroundSupport
+import UIKit
+
+public class Pet {
+    public enum Rarity {
+        case common
+        case uncommon
+        case rare
+        case veryRare
+    }
+    
+    public let name: String
+    public let birthday: Date
+    public let rarity: Rarity
+    public let image: UIImage
+    
+    public init(name: String, birthday: Date, rarity: Rarity, image: UIImage) {
+        self.name = name
+        self.birthday = birthday
+        self.rarity = rarity
+        self.image = image
+    }
+}
+
+public class PetViewModel {
+    private let pet: Pet
+    private let calendar: Calendar
+    
+    public init(pet: Pet, calendar: Calendar) {
+        self.pet = pet
+        self.calendar = calendar
+    }
+    
+    public var name: String {
+        return pet.name
+    }
+    
+    public var image: UIImage {
+        return pet.image
+    }
+    
+    public var ageText: String {
+        let today = calendar.startOfDay(for: Date())
+        let birthday = calendar.startOfDay(for: pet.birthday)
+        
+        let component = calendar.dateComponents([.year], from: today, to: birthday)
+        let age = component.year
+        return "\(age) years old"
+    }
+    
+    public var adoptionFeeText: String {
+        switch pet.rarity {
+        case .common:
+            return "50원"
+        case .uncommon:
+            return "500원"
+        case .rare:
+            return "1000원"
+        case .veryRare:
+            return "5000원"
+        }
+    }
+    
+    public class PetView: UIView{
+        public let imageView: UIImageView
+        public let nameLabel: UILabel
+        public let ageLabel: UILabel
+        public let adoptionFeeLabel: UILabel
+        
+        public override init(frame: CGRect) {
+            var childFrame = CGRect(x: 0, y: 16, width: frame.width, height: frame.height / 2)
+            imageView = UIImageView(frame: childFrame)
+            imageView.contentMode = .scaleAspectFit
+            
+            childFrame.origin.y += childFrame.height + 16
+            childFrame.size.height = 30
+            nameLabel = UILabel(frame: childFrame)
+            nameLabel.textAlignment = .center
+            
+            childFrame.origin.y += childFrame.height
+            ageLabel = UILabel(frame: childFrame)
+            ageLabel.textAlignment = .center
+            
+            childFrame.origin.y += childFrame.height
+            adoptionFeeLabel = UILabel(frame: childFrame)
+            adoptionFeeLabel.textAlignment = .center
+            
+            super.init(frame: frame)
+            backgroundColor = .white
+            addSubview(imageView)
+            addSubview(nameLabel)
+            addSubview(ageLabel)
+            addSubview(adoptionFeeLabel)
+        }
+        
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+    }
+}
